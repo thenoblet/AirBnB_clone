d84ffb351a2c04a9c0d02e5129a6ffeebcf4ce52
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-07 15:03:37 +0000

    added the basics of the HBNBCommand class

---
+++ console.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+""""
+Module: console
+
+Description:
+This is a module that features the HBNBCommand class. The HBNBCommand opens
+a command line interpreter which prompts the user of this program for a
+command. For each command, use the `help` command to list them and how they
+are used.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand - AirBnB command line interface (CLI).
+
+    This class implements a command-line interface (CLI) for an AirBnB clone
+    application. It allows users to interact with the application by
+    entering commands through the console.
+
+    Attributes:
+    - prompt (str): Custom prompt for the command-line interface.
+
+    Methods:
+    do_quit(self, args): Command that exits or quits the console.
+    help_quit(self): Provides information about the quit command.
+    do_EOF(self, args): Command that exits the console (Ctrl+D).
+    help_EOF(self): Provides information about the EOF command.
+    emptyline(self): Upon encountering blank line + `Enter` key, does nothing.
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """
+        Quit command to exit the console.
+
+        Usage: quit
+        """
+        return True
+
+    def help_quit(self):
+        """
+        Provides information about the quit command.
+        """
+        print("Quit command to exit the console\n")
+
+    def do_EOF(self, args):
+        """
+        End of file command (Ctrl+D) that exits the console.
+        """
+        print()
+        return True
+
+    def help_EOF(self):
+        """
+        Provides information about the EOF command
+        """
+        print("Press `Ctrl+D` to exit the console\n")
+
+    def emptyline(self):
+        """
+        Upon encountering a blank line + `Enter`, do nothing
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
