b15be6da02aa1f1090405630fa2f8dd717f82cde
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-10 00:54:55 +0000

    added "place" import

--- console.py
+++ console.py
@@ -39,6 +39,7 @@ from models import storage
 from models.user import User
 from models.state import State
 from models.city import City
+from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
